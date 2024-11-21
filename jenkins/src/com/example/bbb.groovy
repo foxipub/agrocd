ce972eb80a02f9e00f7fef6d0934e253eb16ae9b
foxipub <foxipub@gmail.com>
2024-11-21 16:17:29 +0000

    My commit message

---
+++ jenkins/src/com/example/bbb.groovy
@@ -0,0 +1,9 @@
+package com.example
+
+class TEST {
+
+   def call() {
+         return println("This is how methods work in groovy");
+         println("This is an example of a simple method");
+      } 
+}
