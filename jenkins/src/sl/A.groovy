cd0c686ec3a39e4e0f39570952c8ff0067f1479d
foxipub <foxipub@gmail.com>
2024-11-21 12:55:52 +0000

    My commit message

---
+++ jenkins/src/sl/A.groovy
@@ -0,0 +1,4 @@
+def call() {
+        println("This is how methods work in groovy");
+        println("This is an example of a simple method");
+    } 
--- jenkins/src/sl/TEST.groovy
+++ jenkins/src/sl/TEST.groovy
@@ -5,6 +5,5 @@ class TEST {
    def call() {
          println("This is how methods work in groovy");
          println("This is an example of a simple method");
-         echo "1111";
       } 
 }
